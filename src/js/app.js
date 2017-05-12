e3dea83a90bbf4acb3d80763863302a5f0953b2b
Andy Lui <awlui2014@gmail.com>
2017-05-12 03:53:48 -0700

    feat(remove bar): remove bar
    BREAKING CHANGE: removed bar from obj api

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 export default class foo {
 	constructor() {
-		this.bar = "BAR";
 		this.car = "CAR"; //test new feature
 	}
 }
--- src/js/app.test.js
+++ src/js/app.test.js
@@ -2,7 +2,6 @@ import foo from './app';
 describe('test', () => {
 	it('works', () => {
 		var obj = new foo();
-		console.log(foo.bar);
 	})
 	it('works again', () => {
 		expect(true).to.be.true
